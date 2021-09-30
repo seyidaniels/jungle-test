d823002a174d1bd0b2d0c07b11223445452a3b57
Seyi <seyidaniels@gmail.com>
2021-09-30 14:56:08 +0100

    Base Components Implementation

---
+++ src/components/DateDisplayComponent.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="dates">
+        <p>Show data between</p>
+        <v-row class='mt-6'>
+            <v-select
+          :items="months"
+          label="Start Month"
+          v-model="startMonth"
+        ></v-select>
+        <p>to</p>
+         <v-select
+          :items="months"
+          label="End Month"
+            v-model="endMonth"
+        ></v-select>
+        </v-row>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            months: ["January","February","March","April","May","June","July",
+            "August","September","October","November","December"],
+            startMonth: '',
+            endMonth: '',
+        }
+    }, 
+    watch: {
+        endMonth(value) {
+            if (value && this.startMonth) {
+                //Todo make this 0(1) time complexity
+                this.$emit('update:dateChanged', this.months.indexOf(this.startMonth), this.months.indexOf(this.endMonth))
+            }
+        } 
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.dates {
+    border: 1px solid grey;
+    border-radius: 25px;
+    padding: 10px 20px;
+    p {
+        font-size: 16px;
+        line-height: 18px;
+        text-align: center;
+        color: white;
+    }
+}
+</style>
---
+++ src/components/MonthComponent.vue
@@ -0,0 +1,30 @@
+<template>
+<v-col
+        cols="12"
+        sm="3"
+        
+      >
+        <v-card
+          class="pa-2 rounded-lg month"
+          outlined
+          tile
+        >
+            <p class="month__title"> January </p>
+          <p>Energy Produced: 13253553vh</p>
+          <p>Energy Produced: 13253553vh</p>
+          <p>Energy Produced: 13253553vh</p>
+          <p>Energy Produced: 13253553vh</p>
+        </v-card>
+      </v-col>
+</template>
+
+<style lang="scss" scoped>
+.month {
+    background:#363D4D !important;
+    &__title {
+        text-align: center;
+        color: #fff;
+    }
+}
+</style>
+
